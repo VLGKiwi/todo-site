e73b1a9b73a89730d6556bd0174dc430c4159f5b
Тима <TimofeyKolotovkin2@gmail.com>
2025-12-23 22:09:30 +0300

    feat(domain): add new custom error

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -3,5 +3,6 @@ package domain
 import "errors"
 
 var (
-	ErrNoTitle = errors.New("title is empty")
+	ErrNoTitle      = errors.New("title is empty")
+	ErrTodoNotExist = errors.New("todo with specified id does not exist")
 )
