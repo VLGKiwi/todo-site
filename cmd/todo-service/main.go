124aef6a9b81d2a633069c7a5416048c8c20b3da
Тима <TimofeyKolotovkin2@gmail.com>
2025-12-26 17:35:03 +0300

    feat: implement graceful shutdown

--- cmd/todo-service/main.go
+++ cmd/todo-service/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/FooxyS/todo-service/internal/adapter/memory"
 	"github.com/FooxyS/todo-service/internal/controller/rest"
@@ -11,7 +16,7 @@ import (
 )
 
 func main() {
-	// logger init
+	// LOGGER
 	handler := slog.NewTextHandler(os.Stdout, nil)
 	logger := slog.New(handler)
 	slog.SetDefault(logger)
@@ -19,16 +24,49 @@ func main() {
 	// DB
 	db := memory.New()
 
-	// usecase
+	// USECASE
 	uc := usecase.New(db)
 
-	// http handlers and router
+	// SERVER
 	router := rest.NewRouter(uc)
-
-	slog.Info("start server", "host", "localhost", "port", 8080)
-
-	if err := http.ListenAndServe("localhost:8080", router); err != nil {
-		slog.Error("failed to start server", "error", err)
-		os.Exit(1)
+	server := &http.Server{
+		Addr:         "0.0.0.0:8080",
+		Handler:      router,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  60 * time.Second,
 	}
+
+	// create error channel to receive server errors
+	errCh := make(chan error, 1)
+
+	slog.Info("start server", "addr", server.Addr)
+	go func() {
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			errCh <- err
+		}
+	}()
+
+	// graceful shutdown
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGTERM, syscall.SIGINT)
+
+	select {
+	case err := <-errCh:
+		slog.Error("server failed to start", "error", err)
+		return
+	case sig := <-sigCh:
+		slog.Info("signal received", "signal", sig)
+	}
+
+	slog.Info("shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("http server shutdown error", "error", err)
+	}
+
+	slog.Info("server stopped")
 }
