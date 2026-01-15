f7f38a0d2834a5868038756c6a267cf0b5e5379a
VLGKiwi <zvonarev.dev@gmail.com>
2026-01-15 22:06:57 +0300

    fix: исправил пути апи

--- backend/internal/controller/rest/router.go
+++ backend/internal/controller/rest/router.go
@@ -7,11 +7,11 @@ func NewRouter(usecase UseCase) http.Handler {
 
 	handlers := NewHandlers(usecase)
 
-	mux.HandleFunc("POST /todos", handlers.CreateTodoHandler)
-	mux.HandleFunc("GET /todos", handlers.GetAllTodosHandler)
-	mux.HandleFunc("GET /todos/{id}", handlers.GetTodoHandler)
-	mux.HandleFunc("PUT /todos/{id}", handlers.UpdateTodoHandler)
-	mux.HandleFunc("DELETE /todos/{id}", handlers.DeleteTodoHandler)
+	mux.HandleFunc("POST /api/todos", handlers.CreateTodoHandler)
+	mux.HandleFunc("GET /api/todos", handlers.GetAllTodosHandler)
+	mux.HandleFunc("GET /api/todos/{id}", handlers.GetTodoHandler)
+	mux.HandleFunc("PUT /api/todos/{id}", handlers.UpdateTodoHandler)
+	mux.HandleFunc("DELETE /api/todos/{id}", handlers.DeleteTodoHandler)
 
 	wrappedMux := LoggingMiddleware(mux)
 
